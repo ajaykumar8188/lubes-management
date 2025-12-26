92b8cf6f3aa5dc63dab4179da73d8f61a25bc8ef
webingateteam@gmail.com <webingateteam@gmail.com>
2025-12-26 17:01:59 +0530

    small fixes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -93,7 +93,7 @@ export default function Login() {
             </button>
           </form>
 
-          <div className="mt-6">
+          {/* <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300"></div>
@@ -113,7 +113,7 @@ export default function Login() {
                 <p className="text-green-700">customer@lubes.com / customer123</p>
               </div>
             </div>
-          </div>
+          </div> */}
 
           <p className="mt-6 text-center text-sm text-gray-600">
             Don't have an account?{' '}
